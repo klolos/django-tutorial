a0f886cef6a3df5e23fac0da74b1a83d1b060128
kostis <lolos.kostis@gmail.com>
2016-01-20 19:10:28 +0200

    Project initialization

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/kostis/git/django/venv/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==1.9.1','django-admin.py'
+__requires__ = 'Django==1.9.1'
+__import__('pkg_resources').run_script('Django==1.9.1', 'django-admin.py')
